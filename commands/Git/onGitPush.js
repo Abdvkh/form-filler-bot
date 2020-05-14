c5da815adb62011cbd3cc2d0ae7d25da8d057ef1
Bots.Business <git.sync@bots.business>
2020-05-14 16:42:46 +0000

    Bots.Business export for bot id: 92375

---
+++ commands/Git/onGitPush.js
@@ -0,0 +1,16 @@
+/*CMD
+  command: onGitPush
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: Git
+  answer: Start code import...
+  keyboard: 
+  aliases: 
+CMD*/
+
+Bot.importGit({
+  branch: "master",
+  success: "onGitImportCompleted"
+});
+
