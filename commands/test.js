74a7ef55d3f14376886552b8c12dc322aa46d39e
Abdvkh <e-address@bk.ru>
2020-05-21 23:58:52 +0500

    Added sheet test

---
+++ commands/test.js
@@ -0,0 +1,17 @@
+let sheet = Libs.GoogleSpreadSheet;
+
+let data = {
+   'box': '123',
+   'price': '123',
+   'location': 'Kharkiv',
+   'delivery': 'Tashkent'
+};
+
+let prms = {
+   sheetName: 'Auction',
+   row: data,
+   onSuccess:'onSuccess',
+   onError: onError
+};
+
+sheet.addRow(prms);
