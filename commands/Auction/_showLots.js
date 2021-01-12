4ccf743fbb684678c855a192bee69bfa1cb0840c
abdvkh <e-address@bk.ru>
2021-01-12 16:33:33 +0500

    Add no lots validation;

--- commands/Auction/_showLots.js
+++ commands/Auction/_showLots.js
@@ -10,15 +10,19 @@
 CMD*/
 
 const lots = auction.getLots();
-let msg = 'В текущий момент имеется ' + (lots.length + 1) + ' лотов:\n\n';
+let msg = 'Нету лотов';
+if (lots.length > 0) {
+   msg = 'В текущий момент имеется ' + (lots.length + 1) + ' лотов:\n\n';
 
-lots.sort((a, b) => new Date(a.time) - new Date(a.time))
-    .forEach(lot => {
-      msg += `\nИдентификатор: ` + lot.id;
-      msg += `\nВремя проведения: ` + lot.time;
-      msg += `\nНаименование: ` + lot.title;
-      msg += `\nОписание: ` + lot.description;
-      msg += `\nИдентификатор: ` + lot.startingPrice;
-      msg += `\nОписание беру: ` + lot.title;
-      msg += '\n\n';
-   });
+   lots.sort((a, b) => new Date(a.time) - new Date(a.time))
+       .forEach(lot => {
+          msg += `\nИдентификатор: ` + lot.id;
+          msg += `\nВремя проведения: ` + lot.time;
+          msg += `\nНаименование: ` + lot.title;
+          msg += `\nОписание: ` + lot.description;
+          msg += `\nИдентификатор: ` + lot.startingPrice;
+          msg += `\nОписание беру: ` + lot.title;
+          msg += '\n\n';
+       });
+}
+Bot.sendMessage(msg);
