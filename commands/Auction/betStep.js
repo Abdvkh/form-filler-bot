c330924f58dc4119d6fce2c1714f4ded49ab47c5
abdvkh <e-address@bk.ru>
2021-01-13 17:22:32 +0500

    Code refactoring; Update with new features;

--- commands/Auction/betStep.js
+++ commands/Auction/betStep.js
@@ -9,76 +9,100 @@
   aliases:
 CMD*/
 
-let curBet = auction.getCurBet();
-let gifs = Bot.getProperty('gifs');
-let group = Bot.getProperty('chat');
+const { user, price } = auction.getCurBet();
+const betStep = parseInt(auction.getLotProp('betStep'));
+
+const group = Bot.getProperty('chat');
+
+const betKeyboard = Bot.getProperty('betKeyboard');
+const reply_keyboard = Bot.getProperty('fillFormInlineKeyboard');
 
 if (auction.isOver()) {
-   // sending winner message
-   let reply_keyboard = {
-      inline_keyboard: [
-         [
-            { text: 'Заполнить форму', url: 'tg://resolve?domain=abduvakhidovsbot&start=form' },
-         ],
-      ],
-   };
-   Api.sendMessage({
-      chat_id: curBet['user']['telegramid'],
-      text: lang['msg_to_winner'],
-      reply_markup: reply_keyboard,
-      parse_mode: 'Markdown',
-   });
-   Api.sendMessage({
-      chat_id: group,
-      text: lang['aucOver'] + utils.getLinkFor(curBet['user']),
-      reply_markup: reply_keyboard,
-      parse_mode: 'Markdown',
-   });
-   Api.sendDocument({
-      chat_id: group,
-      document: gifs.file_ids[0]
-   });
+   sendWinnerMessages();
 
-   // sending "Беру"
-   let curAuc = auction.getCurAuction();
+   sendGIF(group, 0);
 
-   Api.sendPhoto({
-      chat_id: group,
-      photo: curAuc['take_picture'],
-      caption: curAuc['take_title'] ? curAuc['take_title'] : "Секция Беру",
-      parse_mode: 'Markdown',
-      reply_markup: reply_keyboard,
-   });
+   sendTakeSection(group, reply_keyboard);
 
-   return Bot.clearRunAfter({
-      label: 'bet'
-   });
+   return Bot.clearRunAfter({label: 'bet'});
 }
 
-let betStep = auction.getCurAuction()['betStep'];
-auction.setCurrentAuction('betStep', parseInt(betStep)+1);
+auction.lot.setLotProp('betStep', betStep + 1);
 
-let betKeyboard = Bot.getProperty('betKeyboard');
-let betStep_str = betStep==1 ? "Раз" : betStep==2 ? "Два" : "Три";
-let betMsg = betStep_str + ' cтавка от ' + utils.getLinkFor(curBet['user']) + ' ' + curBet['price'];
+sendCurrentBetStepMessage();
 
-Api.sendMessage({
-   chat_id: group,
-   text: betMsg,
-   parse_mode: 'Markdown',
-   reply_markup: betKeyboard,
-});
-
-if (betStep==2) {
-   let gif_id = utils.getRandomInt(7, 9);
-   Api.sendDocument({
-      chat_id: group,
-      document: gifs.file_ids[gif_id]
-   });
-}
+sendGIFOnBetStep(betStep)
 
 Bot.run({
    command: 'betStep',
    run_after: 60,
    label: 'bet'
 });
+
+
+/** Sends take section of auction to the given chat with inline reply keyboard
+ * @param {string|number} chatID - Chat ID where it's being sent
+ * @param {object} replyKeyboard - Keyboard attached to message
+ * */
+function sendTakeSection(chatID, replyKeyboard){
+   // sending "Беру"
+   const { takeCaption, takePicture } = auction.getAuction();
+
+   Api.sendPhoto({
+      chat_id: chatID,
+      photo: takePicture,
+      caption: takeCaption || "Секция Беру",
+      parse_mode: 'Markdown',
+      reply_markup: replyKeyboard,
+   });
+}
+
+/** Send GIF regarding current bet step
+ * @param {string|number} betStep - Current bet step
+ * */
+function sendGIFOnBetStep(betStep){
+   if (betStep === 2) {
+      const gifID = utils.getRandomInt(7, 9);
+
+      sendGIF(group, gifID)
+   }
+}
+
+function sendGIF(chatID, index) {
+   const gifs = Bot.getProperty('gifs');
+
+   Api.sendDocument({
+      chat_id: chatID,
+      document: gifs.file_ids[index]
+   });
+}
+
+function sendCurrentBetStepMessage() {
+   const betStepText = betStep === 1 ? "Раз" : betStep === 2 ? "Два" : "Три";
+   const betMsg = betStepText + ' cтавка от ' + utils.getLinkFor(user) + ' ' + price;
+
+   Api.sendMessage({
+      chat_id: group,
+      text: betMsg,
+      parse_mode: 'Markdown',
+      reply_markup: betKeyboard,
+   });
+}
+
+function sendWinnerMessages() {
+   const { msgToWinner, auctionOver } = lang;
+   const { telegramid } = user;
+   const messages = [
+      [telegramid, msgToWinner],
+      [group, auctionOver + utils.getLinkFor(user)],
+   ];
+
+   messages.forEach(([chatID, message]) => {
+      Api.sendMessage({
+         chat_id: chatID,
+         text: message,
+         reply_markup: reply_keyboard,
+         parse_mode: 'Markdown',
+      });
+   });
+}
