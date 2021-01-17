96d7ca47d283016cc957ee1d3c99cf0d789dc72b
abdvkh <e-address@bk.ru>
2021-01-17 11:09:55 +0500

    Stop auction feature

--- commands/Auction/stopAuction.js
+++ commands/Auction/stopAuction.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /template
+  command: stopAuction
   help:
   need_reply:
   auto_retry_time:
@@ -8,3 +8,26 @@
   keyboard: Главное меню
   aliases:
 CMD*/
+
+
+const auctionID = params || options['auctionID'];
+const { id } = auction.getAuction();
+const { noSuchAuction, stopped } = lang['auction'];
+
+if (id === auctionID){// if auction id is the same as current auction ID
+    Api.sendMessage({
+        text: stopped.replace('{auctionID}', auctionID)
+    });
+    stopAuction(auctionID);
+} else {
+    Api.sendMessage({
+        text: noSuchAuction.replace('{auctionID}', auctionID)
+    });
+}
+Bot.run({command: '/showAuctions'});
+
+
+function stopAuction(auctionID) {
+    auction.setAucProp('status', 'stopped', null, auctionID);
+    Bot.crearRunAfter({label: 'startAcution' + auctionID});
+}
