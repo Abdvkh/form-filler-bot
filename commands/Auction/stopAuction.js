09170e357a3dfdb21b4fdb92594581e988d61b13
abdvkh <e-address@bk.ru>
2021-01-18 19:42:57 +0500

    Debugging; Refactoring; Stop auction feature corrected;

--- commands/Auction/stopAuction.js
+++ commands/Auction/stopAuction.js
@@ -5,27 +5,32 @@
   auto_retry_time:
   folder: Auction
   answer:
-  keyboard: Главное меню
+  keyboard:
   aliases:
 CMD*/
 
+let msg;
 const auctionID = params || options['auctionID'];
-const { id } = auction.getAuction();
+const auctionExists = auction.getAucByID(auctionID);
 const { noSuchAuction, stopped } = lang['auction'];
 
-if (id === auctionID){// if auction id is the same as current auction ID
-    Api.sendMessage({
-        text: stopped.replace('{auctionID}', auctionID)
-    });
+if (auctionExists){// if auction id is the same as current auction ID
+    msg = stopped.replace('{auctionID}', auctionID);
     stopAuction(auctionID);
 } else {
-    Api.sendMessage({
-        text: noSuchAuction.replace('{auctionID}', auctionID)
-    });
+    msg = noSuchAuction.replace('{auctionID}', auctionID)
 }
-Bot.run({command: '/showAuctions'});
+
+utils.runCommandWithKeyboard({
+    cmd: '/showAuctions',
+    txt: msg,
+    btns: []
+}, 'm');
 
 
+/** Turns auction status to be stopped and clears running auction
+ * @param {string} auctionID - Auction ID
+ * */
 function stopAuction(auctionID) {
     auction.setAucProp('status', 'stopped', null, auctionID);
     Bot.crearRunAfter({label: 'startAcution' + auctionID});
