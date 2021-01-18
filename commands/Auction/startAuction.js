39cbd7ded653129ec663bc41aeb92ec8dc13ebb2
abdvkh <e-address@bk.ru>
2021-01-18 13:32:20 +0500

    Debugging

--- commands/Auction/startAuction.js
+++ commands/Auction/startAuction.js
@@ -19,7 +19,7 @@ if (auction.setupCurAuc(auctionID) && auction.getAucLotsCount() > 0){// if aucti
       text: noSuchAuction.replace('{auctionID}', auctionID)
    });
 }
-options.length > 0 && Bot.run({command: '/showAuctions'});// if has options run command
+options && Bot.run({command: '/showAuctions'});// if has options run command
 
 
 function startAuction() {
